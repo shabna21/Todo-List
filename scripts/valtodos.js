a0f162c31a11c55a0c8e7a003c63241cbdedc101
shabna <shabnabenadict06@gmail.com>
2021-02-24 10:52:44 +0530

    valtodos modified

--- scripts/valtodos.js
+++ scripts/valtodos.js
@@ -5,9 +5,11 @@ function checkboxes()
 for (var i=0; i<inputElems.length; i++) {       
     if (inputElems[i].type == "checkbox" && inputElems[i].checked == true){    
        count++;
-         }
-    
- }
-alert("You have successfully completed the task!!");
+       var cnt=count;
+      }         
+}
+if(cnt==95){
+alert("You have successfully completed 5 tasks!!");
+}
 
 }
